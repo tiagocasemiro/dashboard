d0e2fefc58422ba6cca69f2e3478bab39a387162
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-12 10:03:27 -0300

    add font style

---
+++ app/src/main/java/com/dashboard/style.kt
@@ -0,0 +1,14 @@
+package com.dashboard
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+
+val firaSansFamily = FontFamily(
+    Font(R.font.firasans_light, FontWeight.Light),
+    Font(R.font.firasans_regular, FontWeight.Normal),
+    Font(R.font.firasans_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.firasans_medium, FontWeight.Medium),
+    Font(R.font.firasans_bold, FontWeight.Bold)
+)
