c57a1140cb7eec931eb1037ee72d9f366f165b31
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-13 09:56:33 -0300

    create first usecase

---
+++ app/src/main/java/com/dashboard/model/usecase/MainPageUseCase.kt
@@ -0,0 +1,6 @@
+package com.dashboard.model.usecase
+
+import com.dashboard.repository.DashboardRemoteRepository
+
+class MainPageUseCase(private val remoteRepository: DashboardRemoteRepository) {
+}
