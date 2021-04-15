ca11da73c4aefbbeb1883159484d4b01ad639498
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-15 10:40:38 -0300

    modifier cover

--- app/src/main/java/com/dashboard/view/screen/CoverScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/CoverScreen.kt
@@ -6,54 +6,62 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.dashboard.R
-import com.dashboard.view.components.TopBar
+import com.dashboard.view.components.MainCardNews
+import com.dashboard.view.components.TopBarWithSearch
 import com.dashboard.view.firaSansFamily
 
 @Composable
 fun CoverScreen(navController: NavController) {
-
-        Scaffold(
-            topBar = {
-                TopBar(R.string.app_name)
-            }
-        ) {
-           Column {
-                RowListText(listOf("Política", "Esporte","Cinema", "Lazer"))
-                Button(onClick = {
-                    navController.navigate("search-screen")
-                }) {
-                    Text(text = "ir")
-                }
-            }
+    Scaffold(
+        topBar = {
+            TopBarWithSearch(R.string.app_name, onSearch = {
+                navController.navigate("search-screen")
+            })
         }
-
+    ) {
+       Column {
+            RowListText(listOf("Política", "Esporte", "Cinema", "Lazer"))
+            MainCardNews(null)
+        }
+    }
 }
 
 @Composable
 fun RowListText(messages: List<String>) {
-    LazyRow(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-        horizontalArrangement = Arrangement.spacedBy(16.dp),) {
-        items(messages) { message ->
-            Text(
-                text = message,
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                fontFamily = firaSansFamily,
-                modifier = Modifier.fillMaxWidth(),
-            )
+    Column {
+        LazyRow(
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),) {
+            items(messages) { message ->
+                Text(
+                    text = message,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = firaSansFamily,
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
         }
+        Divider(color = colorResource(id = R.color.divider), thickness = 1.dp)
     }
+}
+
+@Preview
+@Composable
+fun preview() {
+    RowListText(listOf("Política", "Esporte","Cinema", "Lazer"))
 }
 
