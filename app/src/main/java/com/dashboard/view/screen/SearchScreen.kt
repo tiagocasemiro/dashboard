0a3e8b8b7b591a092af810259620a8c689bd9acf
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-15 10:40:09 -0300

    prepare search screen

--- app/src/main/java/com/dashboard/view/screen/SearchScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/SearchScreen.kt
@@ -1,29 +1,22 @@
 package com.dashboard.view.screen
 
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.dashboard.R
-import com.dashboard.view.components.TopBar
-import com.dashboard.view.components.TopBarBackAndFindButtons
+import com.dashboard.view.components.TopBarWithBack
 
 @Composable
 fun SearchScreen(navController: NavController) {
     Scaffold(
         topBar = {
-            TopBarBackAndFindButtons(R.string.search_screen_title,
+            TopBarWithBack(R.string.search_screen_title,
             onBack = {
                 navController.navigate("cover-screen") {
                     popUpTo = navController.graph.startDestination
                     launchSingleTop = true
                 }
-            },
-            onSearch = {
-
             })
         }
     ) {
