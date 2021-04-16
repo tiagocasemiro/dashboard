3865a5fb3ce0bbef62eea44152ea37d9eb8d6782
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-16 10:05:28 -0300

    add preview

--- app/src/main/java/com/dashboard/view/MainActivity.kt
+++ app/src/main/java/com/dashboard/view/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.dashboard.view
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import coil.Coil
+import coil.util.CoilUtils
 import com.dashboard.view.screen.CoverScreen
 import com.dashboard.view.screen.SearchScreen
 
 //https://www.behance.net/gallery/85077173/Sector-News
+//https://github.com/android/compose-samples/tree/master/JetNews
+//https://github.com/google/accompanist
 class MainActivity: ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +30,24 @@ class MainActivity: ComponentActivity() {
         }
     }
 
-      @Composable
-      fun NavigationInCompose() {
-          val navController = rememberNavController()
-          NavHost(
-              navController = navController,
-              startDestination = "cover-screen") {
-              composable("cover-screen") {
-                  CoverScreen(navController = navController)
-              }
-              composable("search-screen") {
-                  SearchScreen(navController = navController)
-              }
-         }
-      }
+    @Composable
+    fun NavigationInCompose() {
+        val navController = rememberNavController()
+        NavHost(
+            navController = navController,
+            startDestination = "cover-screen") {
+            composable("cover-screen") {
+                CoverScreen(navController = navController)
+            }
+            composable("search-screen") {
+                SearchScreen(navController = navController)
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun preview() {
+        NavigationInCompose()
+    }
 }
