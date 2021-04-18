7bae399cf1b5f45930d3944067237b362cd896c6
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-18 19:24:23 -0300

    init view model

--- app/src/main/java/com/dashboard/view/MainActivity.kt
+++ app/src/main/java/com/dashboard/view/MainActivity.kt
@@ -1,26 +1,27 @@
 package com.dashboard.view
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import coil.Coil
-import coil.util.CoilUtils
-import com.dashboard.view.screen.CoverScreen
-import com.dashboard.view.screen.SearchScreen
+import com.dashboard.view.screen.cover.CoverScreen
+import com.dashboard.view.screen.search.SearchScreen
+import com.dashboard.view.screen.cover.CoverViewModel
+import com.dashboard.view.screen.search.SearchViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
 
 //https://www.behance.net/gallery/85077173/Sector-News
 //https://github.com/android/compose-samples/tree/master/JetNews
 //https://github.com/google/accompanist
 class MainActivity: ComponentActivity() {
+    private val coverViewModel: CoverViewModel by viewModel()
+    private val searchViewModel: SearchViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -37,10 +38,10 @@ class MainActivity: ComponentActivity() {
             navController = navController,
             startDestination = "cover-screen") {
             composable("cover-screen") {
-                CoverScreen(navController = navController)
+                CoverScreen(navController = navController, viewModel = coverViewModel)
             }
             composable("search-screen") {
-                SearchScreen(navController = navController)
+                SearchScreen(navController = navController, viewModel = searchViewModel)
             }
         }
     }
