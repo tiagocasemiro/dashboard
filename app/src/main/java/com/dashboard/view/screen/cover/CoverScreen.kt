996965ec65846dc85f62aec0670a83150ffa5857
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-18 19:24:04 -0300

    Construction cover screen

--- app/src/main/java/com/dashboard/view/screen/cover/CoverScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/cover/CoverScreen.kt
@@ -1,29 +1,26 @@
-package com.dashboard.view.screen
+package com.dashboard.view.screen.cover
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.dashboard.R
-import com.dashboard.view.components.DashboardDivider
+import com.dashboard.model.domain.Article
+import com.dashboard.model.domain.Source
 import com.dashboard.view.components.MainCardNews
+import com.dashboard.view.components.RowListText
+import com.dashboard.view.components.SourcesCard
 import com.dashboard.view.components.TopBarWithSearch
-import com.dashboard.view.firaSansFamily
 
 @Composable
-fun CoverScreen(navController: NavController) {
+fun CoverScreen(navController: NavController, viewModel: CoverViewModel) {
     Scaffold(
         topBar = {
             TopBarWithSearch(R.string.app_name, onSearch = {
@@ -33,30 +30,27 @@ fun CoverScreen(navController: NavController) {
     ) {
        Column {
             RowListText(listOf("Política", "Esporte", "Cinema", "Lazer", "Política", "Esporte","Cinema", "Lazer"))
-            MainCardNews(null)
+            CoverNews(listOf(), listOf())
         }
     }
 }
 
 @Composable
-fun RowListText(messages: List<String>) {
-    Column {
-        LazyRow(
-            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),) {
-            items(messages) { message ->
-                Text(
-                    text = message,
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = firaSansFamily,
-                    modifier = Modifier.fillMaxWidth(),
-                )
-            }
+fun CoverNews(articles: List<Article>, sources: List<Source>) {
+    LazyColumn(
+        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+    ) {
+        items(articles) { article ->
+             MainCardNews(article)
         }
-        DashboardDivider()
+        item {
+            SourcesCard(sources)
+        }
     }
 }
+
+
 
 @Preview
 @Composable
