152bd99e774ed34fc1de0477b5c6ffae1a19ac33
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-19 12:10:44 -0300

    implement article

--- app/src/main/java/com/dashboard/view/screen/cover/CoverScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/cover/CoverScreen.kt
@@ -3,10 +3,14 @@ package com.dashboard.view.screen.cover
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -14,13 +18,14 @@ import androidx.navigation.compose.navigate
 import com.dashboard.R
 import com.dashboard.model.domain.Article
 import com.dashboard.model.domain.Source
-import com.dashboard.view.components.MainCardNews
-import com.dashboard.view.components.RowListText
-import com.dashboard.view.components.SourcesCard
-import com.dashboard.view.components.TopBarWithSearch
+import com.dashboard.view.components.*
 
 @Composable
 fun CoverScreen(navController: NavController, viewModel: CoverViewModel) {
+    val stateArticles: MutableState<CoverState> = remember { mutableStateOf(CoverState.Empty) }
+    viewModel.article {
+        stateArticles.value = it
+    }
     Scaffold(
         topBar = {
             TopBarWithSearch(R.string.app_name, onSearch = {
@@ -28,9 +33,20 @@ fun CoverScreen(navController: NavController, viewModel: CoverViewModel) {
             })
         }
     ) {
-       Column {
+        Column{
             RowListText(listOf("Política", "Esporte", "Cinema", "Lazer", "Política", "Esporte","Cinema", "Lazer"))
-            CoverNews(listOf(), listOf())
+            when(stateArticles.value) {
+                is CoverState.Data -> {
+                    val state = stateArticles.value as State<CoverState.Data>
+                    CoverNews(state.value.articles, listOf())
+                }
+                is CoverState.Empty -> {
+                    CoverEmpty()
+                }
+                is CoverState.Error -> {
+                    PresentDialog()
+                }
+            }
         }
     }
 }
@@ -42,7 +58,7 @@ fun CoverNews(articles: List<Article>, sources: List<Source>) {
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         items(articles) { article ->
-             MainCardNews(article)
+            MainCardNews(article)
         }
         item {
             SourcesCard(sources)
@@ -51,6 +67,16 @@ fun CoverNews(articles: List<Article>, sources: List<Source>) {
 }
 
 
+@Composable
+fun CoverEmpty() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "No data")
+    }
+}
 
 @Preview
 @Composable
