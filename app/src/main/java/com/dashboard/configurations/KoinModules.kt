150df63af4f67ec0ad480a164d0a058c8c3da5cb
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-19 12:09:42 -0300

    ajust injection

--- app/src/main/java/com/dashboard/configurations/KoinModules.kt
+++ app/src/main/java/com/dashboard/configurations/KoinModules.kt
@@ -7,21 +7,22 @@ import com.dashboard.repository.DashboardRemoteRepository
 import com.dashboard.repository.implementation.DashboardRemoteRepositoryImplementation
 import com.dashboard.repository.remote.DashboardGateway
 import com.dashboard.view.screen.cover.CoverViewModel
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-import javax.net.ssl.HostnameVerifier
 
 var retrofitModule = module {
-    factory {
+    factory<Retrofit> {
         val host = if(BuildConfig.DEBUG) {
-            "https://sub.stg.m4u.com.br/salesman/"
+            "https://sheltered-dawn-75915.herokuapp.com/"
         } else {
-            "https://sub.m4u.com.br/salesman/"
+            "https://soccer-news-gatway.herokuapp.com/"
         }
         Retrofit.Builder()
             .baseUrl(host)
@@ -33,27 +34,28 @@ var retrofitModule = module {
                     .addInterceptor(HttpLoggingInterceptor().apply {
                         level = HttpLoggingInterceptor.Level.BODY
                     })
-                    .hostnameVerifier(HostnameVerifier { hostname, _ ->
-                        host.contains(hostname) || BuildConfig.DEBUG
+                    .addInterceptor(object: Interceptor {
+                        override fun intercept(chain: Interceptor.Chain): Response {
+                            println("*** -> " + chain.request().url.encodedPath)
+
+                            return chain.proceed(chain.request())
+                        }
                     })
+                    .hostnameVerifier { hostname, _ ->
+                        host.contains(hostname) || BuildConfig.DEBUG
+                    }
                     .build()
             )
             .build()
     }
-    factory {
+    factory<DashboardGateway> {
         get<Retrofit>().create(DashboardGateway::class.java)
     }
-    factory {
-        MainPageUseCase(get())
-    }
-    factory<DashboardRemoteRepository> {
-        DashboardRemoteRepositoryImplementation(get())
-    }
-    factory {
-        TaskExecutor()
-    }
+}
+
+var viewModelModule = module {
     viewModel {
-        val task: TaskExecutor = get()
+        val task = TaskExecutor()
         val coverViewModel = CoverViewModel(
             repository = get(),
             task = task
@@ -61,5 +63,17 @@ var retrofitModule = module {
         task.with(coverViewModel.viewModelScope)
 
         coverViewModel
+    }
+}
+
+var repositoryModule = module {
+    factory<DashboardRemoteRepository> {
+        DashboardRemoteRepositoryImplementation(get())
+    }
+}
+
+var useCaseModule = module {
+    factory {
+        MainPageUseCase(get())
     }
 }
