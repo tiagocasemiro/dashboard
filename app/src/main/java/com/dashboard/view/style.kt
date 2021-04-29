d5cd5c2d2e3677bbe4562cd98adb2ef3ed566130
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-29 10:25:40 -0300

    add shape

--- app/src/main/java/com/dashboard/view/style.kt
+++ app/src/main/java/com/dashboard/view/style.kt
@@ -1,10 +1,13 @@
 package com.dashboard.view
 
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.Shapes
 import com.dashboard.R
 
 val dashboardPadding = 15.dp
@@ -17,4 +20,10 @@ val firaSansFamily = FontFamily(
     Font(R.font.firasans_italic, FontWeight.Normal, FontStyle.Italic),
     Font(R.font.firasans_medium, FontWeight.Medium),
     Font(R.font.firasans_bold, FontWeight.Bold)
+)
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(percent = 50),
+    medium = RoundedCornerShape(50),
+    large = RoundedCornerShape(8f)
 )
