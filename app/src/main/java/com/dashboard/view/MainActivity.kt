9ae8fcfadd024813048c25e7812d69ca5b63cf8d
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-04-30 08:43:24 -0300

    Start srticles

--- app/src/main/java/com/dashboard/view/MainActivity.kt
+++ app/src/main/java/com/dashboard/view/MainActivity.kt
@@ -10,10 +10,14 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.dashboard.view.screen.cover.CoverScreen
+import com.dashboard.view.screen.cover.CoverState
 import com.dashboard.view.screen.search.SearchScreen
 import com.dashboard.view.screen.cover.CoverViewModel
 import com.dashboard.view.screen.search.SearchViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.State
+
 
 //https://www.behance.net/gallery/85077173/Sector-News
 //https://github.com/android/compose-samples/tree/master/JetNews
@@ -25,6 +29,7 @@ class MainActivity: ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        coverViewModel.articles()
         setContent {
             MaterialTheme {
                 NavigationInCompose()
@@ -35,11 +40,13 @@ class MainActivity: ComponentActivity() {
     @Composable
     fun NavigationInCompose() {
         val navController = rememberNavController()
+        val stateArticles: State<CoverState> = coverViewModel.stateArticles.observeAsState(CoverState.Load)
+
         NavHost(
             navController = navController,
             startDestination = "cover-screen") {
             composable("cover-screen") {
-                CoverScreen(navController = navController, viewModel = coverViewModel)
+                CoverScreen(navController = navController, stateArticles = stateArticles)
             }
             composable("search-screen") {
                 SearchScreen(navController = navController, viewModel = searchViewModel)
