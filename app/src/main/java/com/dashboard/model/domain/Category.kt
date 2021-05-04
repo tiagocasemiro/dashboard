0180247ea0b4592cf4b0e1badfca9ca270c4e43d
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-05-04 08:44:52 -0300

    Add categorie translate

--- app/src/main/java/com/dashboard/model/domain/Category.kt
+++ app/src/main/java/com/dashboard/model/domain/Category.kt
@@ -3,7 +3,7 @@ package com.dashboard.model.domain
 import com.dashboard.repository.remote.DomainMapperResponse
 import com.google.gson.annotations.SerializedName
 
-data class Category(@SerializedName("name") val name: String)
+data class Category(@SerializedName("name") val name: String, @SerializedName("name-pt-br") val namePtBr: String)
 
 data class Categories(
     @SerializedName("status") val status : String,
