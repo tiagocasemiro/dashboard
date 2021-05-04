daf010c7d9ed1012a4fa070937e0f0f9d864ec2f
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-05-04 08:45:55 -0300

    add new integrations

--- app/src/main/java/com/dashboard/view/screen/cover/CoverViewModel.kt
+++ app/src/main/java/com/dashboard/view/screen/cover/CoverViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dashboard.configurations.TaskExecutor
 import com.dashboard.model.domain.Article
+import com.dashboard.model.domain.Category
 import com.dashboard.model.domain.Source
 import com.dashboard.repository.DashboardRemoteRepository
 import com.dashboard.repository.remote.onFailure
@@ -16,11 +17,27 @@ class CoverViewModel(private val repository: DashboardRemoteRepository, private 
 
    fun articles() = task.exec {
        val result = task.async { repository.headlines() }
+       val categories = task.async { repository.categories() }
+       val sources = task.async { repository.sources() }
+
        result?.onSuccess {
-           if(it.articles.isEmpty())
+           if(it.articles.isEmpty()) {
                _stateArticles.value = CoverState.Empty
-           else
-               _stateArticles.value = CoverState.Data(it.articles, listOf(Source(name ="Globo"), Source(name ="Band"), Source(name ="Nexo")))
+           } else {
+               categories?.onSuccess { categoriesResult ->
+                   sources?.onSuccess { sourcesResult ->
+                       _stateArticles.value = CoverState.Data(
+                           it.articles,
+                           sourcesResult.sources,
+                           categoriesResult.categories
+                       )
+                   }?.onFailure {
+                       _stateArticles.value = CoverState.Error(it?.message?:"")
+                   }
+               }?.onFailure {
+                   _stateArticles.value = CoverState.Error(it?.message?:"")
+               }
+           }
        }?.onFailure {
            _stateArticles.value = CoverState.Error(it?.message?:"")
        }
@@ -31,5 +48,5 @@ sealed class CoverState {
     object Empty : CoverState()
     object Load : CoverState()
     class Error(val message: String) : CoverState()
-    class Data(val articles: List<Article>, val sources: List<Source>) : CoverState()
+    class Data(val articles: List<Article>, val sources: List<Source>, val categories: List<Category>) : CoverState()
 }
