eab99e81a5d4ce3085ecc850a661f36500ec5315
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-05-07 09:54:59 -0300

    Iniciado tela de busca

--- app/src/main/java/com/dashboard/view/screen/search/SearchScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/search/SearchScreen.kt
@@ -1,25 +1,29 @@
 package com.dashboard.view.screen.search
 
+import androidx.compose.foundation.layout.Column
+import com.dashboard.view.components.TopBarWithBack
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.dashboard.R
-import com.dashboard.view.components.TopBarWithBack
 
 @Composable
 fun SearchScreen(navController: NavController, viewModel: SearchViewModel) {
     Scaffold(
         topBar = {
             TopBarWithBack(R.string.search_screen_title,
-            onBack = {
-                navController.navigate("cover-screen") {
-                    popUpTo = navController.graph.startDestination
-                    launchSingleTop = true
+                onBack = {
+                    navController.navigate("cover-screen") {
+                        popUpTo = navController.graph.startDestination
+                        launchSingleTop = true
+                    }
                 }
-            })
+            )
         }
     ) {
+        Column {
 
+        }
     }
 }
