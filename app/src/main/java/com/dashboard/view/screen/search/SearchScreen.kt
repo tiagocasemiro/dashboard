6fa6e46fffe2d5149bfd53e87a0096e764c9605f
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-05-18 09:23:48 -0300

    implement part view model

--- app/src/main/java/com/dashboard/view/screen/search/SearchScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/search/SearchScreen.kt
@@ -4,13 +4,18 @@ import androidx.compose.foundation.layout.Column
 import com.dashboard.view.components.TopBarWithBack
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.dashboard.R
 import com.dashboard.view.components.DashboardTextField
+import com.dashboard.view.screen.cover.CoverState
 
 @Composable
 fun SearchScreen(navController: NavController, viewModel: SearchViewModel) {
+    val stateArticles: State<SearchState> = viewModel.stateArticles.observeAsState(SearchState.Empty)
+
     Scaffold(
         topBar = {
             TopBarWithBack(R.string.search_screen_title,
@@ -25,6 +30,8 @@ fun SearchScreen(navController: NavController, viewModel: SearchViewModel) {
     ) {
         Column {
             DashboardTextField()
+
+
         }
     }
 }
