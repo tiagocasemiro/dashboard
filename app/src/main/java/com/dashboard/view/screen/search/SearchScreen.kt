9fab65ed71526373bdb5f509c3e9efbd0e5f539b
tiagocasemiro <tiagocasemiro@hotmail.com>
2021-05-19 10:25:15 -0300

    implement list news

--- app/src/main/java/com/dashboard/view/screen/search/SearchScreen.kt
+++ app/src/main/java/com/dashboard/view/screen/search/SearchScreen.kt
@@ -10,6 +10,11 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
 import com.dashboard.R
 import com.dashboard.view.components.DashboardTextField
+import com.dashboard.view.components.ErrorDialog
+import com.dashboard.view.components.RowListText
+import com.dashboard.view.screen.cover.CoverEmpty
+import com.dashboard.view.screen.cover.CoverLoad
+import com.dashboard.view.screen.cover.CoverNews
 import com.dashboard.view.screen.cover.CoverState
 
 @Composable
@@ -30,8 +35,21 @@ fun SearchScreen(navController: NavController, viewModel: SearchViewModel) {
     ) {
         Column {
             DashboardTextField()
-
-
+            when(stateArticles.value) {
+                is SearchState.Data -> {
+                    RowListText((stateArticles.value as CoverState.Data).categories.map { it.namePtBr } )
+                    CoverNews(stateArticles.value as CoverState.Data)
+                }
+                is SearchState.Empty -> {
+                    CoverEmpty()
+                }
+                is SearchState.Error -> {
+                    ErrorDialog("Ops, tivemos um problema","Erro ao tentar consultar Artigos. Tente novamente mais tarde.")
+                }
+                is SearchState.Load -> {
+                    CoverLoad()
+                }
+            }
         }
     }
 }
